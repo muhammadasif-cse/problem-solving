62add1788f274275a933b559e08fc0aafd6fd3dc
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-04-09 22:26:45 +0600

    fix: correct output format description in i-love-practice.c

--- C-problem-solving/basic/i-love-practice.c
+++ C-problem-solving/basic/i-love-practice.c
@@ -8,7 +8,7 @@ Statement
 I know that you're loving practice days! So this task is for you. You need to print "I Love Practice" without the quotation marks. I know that you can do it!
 
 Input format
-No input..
+No input
 
 Output Format
 Output "I Love Practice"
