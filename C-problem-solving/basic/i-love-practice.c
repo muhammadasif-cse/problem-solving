824f4ddf05387c68b797f72393be7deede1c0686
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-04-09 22:24:39 +0600

    [START] - problem solving with c

---
+++ C-problem-solving/basic/i-love-practice.c
@@ -0,0 +1,27 @@
+/*
+I Love Practice
+Time Limit: 1 Seconds
+
+Memory Limit: 2.93 MB
+
+Statement
+I know that you're loving practice days! So this task is for you. You need to print "I Love Practice" without the quotation marks. I know that you can do it!
+
+Input format
+No input..
+
+Output Format
+Output "I Love Practice"
+
+Sample Input 1
+
+Sample Output 1
+I Love Practice
+*/
+
+#include <stdio.h>
+int main () {
+  printf("I Love Practice");
+  return 0;
+
+  }
