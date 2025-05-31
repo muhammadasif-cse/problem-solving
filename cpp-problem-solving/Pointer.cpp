4c5cd4eda90920d05980da2b018df3ae4d10c891
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-05-31 14:10:05 +0600

    Add implementations for "Arrays Introduction" and "Pointer" problems from HackerRank

---
+++ cpp-problem-solving/Pointer.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+    cin.ignore();
+    cin >> b;
+
+    cout << (a + b) << endl
+         << abs(a - b) << endl;
+    return 0;
+}
